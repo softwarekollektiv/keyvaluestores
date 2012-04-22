4b1cf21f5aaf4b01a368bcdbf8c840b489f14ec0
booo <borgers@mi.fu-berlin.de>
2012-04-22 15:05:33 +0200

    demo: initial commit for the demo application

---
+++ demo/README.md
@@ -0,0 +1,12 @@
+# The demo app
+
+## Flask
+
+* [Quickstart](http://flask.pocoo.org/docs/quickstart/)
+
+## Install & Startup
+
+    virtualenv env
+    . env/bin/activate
+    pip install flask
+    python app.py
---
+++ demo/app.py
@@ -0,0 +1,83 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from datetime import datetime
+
+import riak
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    blogs = [
+            {'id': 'philipps_crazy_blog'},
+            {'id': 'felix_crazy_blog'}
+            ]
+    posts = [
+            {'blog_id': 'philipps_crazy_blog', 'title': 'lala', 'timestamp': 123456},
+            {'blog_id': 'felix_crazy_blog', 'title': 'foo', 'timestamp': 123456},
+            {'blog_id': 'philipps_cray_blog', 'title': 'bar', 'timestamp': 123456}
+            ]
+    return render_template('index.html', blogs=blogs, posts=posts)
+
+@app.route('/blogs/<title>/')
+def blogs(title):
+    blog = {
+            'title': title,
+            'posts': [{'title': 'lala'}]
+           }
+    return render_template('blog.html', blog=blog)
+
+@app.route('/users/<username>/')
+def users(username):
+    return render_template('user.html', username=username)
+
+@app.route('/blogs/<blog_title>/posts/', methods=['POST'])
+@app.route('/blogs/<blog_title>/posts/<post_title>/')
+def posts(blog_title, post_title=None):
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        if title != None and body != None:
+            #blog = Blog(blog_title)
+            #post = Post(request.form['title'], request.form['body'], blog)
+
+            #client = riak.RiakClient()
+            #bucket = client.bucket('posts')
+            #post = bucket.new(blog_title + "-" + title, {'title': title, 'body': body, 'timestamp': 12345})
+            #post.store()
+
+            print(client)
+
+        else:
+            return ""
+    else:
+        return 'Post %s' % post_title
+
+if __name__ == '__main__':
+
+    app.debug = True
+    app.run()
+
+
+
+# classes & models
+
+class Post(object):
+
+    def __init__(self, title, body, blog):
+        self.blog = blog
+        self.title = title
+        self.body = body
+
+class Blog(object):
+
+    def __init__(self, title, author=None):
+        self.title = title
+        self.author = author
+
+    def getPosts(self):
+        pass
